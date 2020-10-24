689a02541c146bed013b0feaff4e3b5078651f35
dxr32 <dx.r32@hotmail.com>
2020-10-24 10:43:32 -0700

    Initial commit

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sens = 100f;
+    float xRotation = 0f;
+
+    public Transform player;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed;
+    float pSpeed;
+    public float jumpHeight;
+    public float gravity = -20f;
+    public float groundDistance = 0.4f;
+    public float ceelingDistance = 1f;
+
+    public Transform ground;
+    public Transform ceeling;
+    public Transform ceelingMax;
+    
+    public LayerMask groundMask;
+    public LayerMask enviroment;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+    bool canStand;
+    bool crouching;
+    bool running;
+
+    void Start()
+    {
+        pSpeed = speed;
+    }
+
+    void Update()
+    {
+        Movement();
+        Gravity();
+        Jump();
+        Run();
+        Crouch();
+    }
+
+    void Gravity()
+    {
+        isGrounded = Physics.CheckSphere(ground.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0f)
+            velocity.y = -2f;
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Movement()
+    {
+        float xAxis = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float zAxis = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector3 move = transform.right * xAxis + transform.forward * zAxis;
+
+        controller.Move(move);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded && !canStand)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+
+    void Run()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !crouching)
+        {
+            speed *= 1.5f;
+            running = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift) && !crouching)
+        {
+            speed = pSpeed;
+            running = false;
+        }
+    }
+
+    void Crouch()
+    {
+        canStand = Physics.CheckCapsule(ceeling.position, ceelingMax.position, 0.1f, enviroment);
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !canStand && !running)
+        {
+            controller.height = 0.5f;
+            controller.radius = 0f;
+            speed /= 3f;
+            crouching = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl) && !canStand && !running)
+        {
+            controller.height = 2f;
+            controller.radius = 0.5f;
+            speed /= 3f;
+            crouching = false;
+        }else if(!Input.GetKey(KeyCode.LeftControl) && !canStand && !running)
+        {
+            controller.height = 2f;
+            controller.radius = 0.5f;
+            speed = pSpeed;
+            crouching = false;
+        }
+    }
+}
