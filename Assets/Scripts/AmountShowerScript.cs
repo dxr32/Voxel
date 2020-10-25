b51114052f8e4831ee9570d9226330ba471cac71
Marcin33K <73389814+Marcin33K@users.noreply.github.com>
2020-10-25 15:10:35 +0100

    Add files via upload

---
+++ Assets/Scripts/AmountShowerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmountShowerScript : MonoBehaviour
+{
+    public Text[] amount;
+
+    public InventorySystem inv;
+
+    public string[] amountS;
+
+    void Update()
+    {
+        for (int i = 0; i < inv.amount.Length; i++)
+        {
+            amountS[i] = "" + inv.amount[i];
+            amount[i].text = amountS[i];
+        }
+    }
+}
---
+++ Assets/Scripts/InventorySystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    public int[] amount;
+    public int[] maxAmount;
+    public int selected;
+
+    public bool[] isFull;
+
+    public GameObject[] slots;
+    public GameObject[] inSlot;
+
+    public Transform changer;
+
+    private void Update()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selected >= slots.Length - 1)
+                selected = 0;
+            else
+                selected++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selected <= 0)
+                selected = slots.Length - 1;
+            else
+                selected--;
+        }
+
+        changer.position = new Vector3(slots[selected].transform.position.x, slots[selected].transform.position.y -45f, slots[selected].transform.position.z);
+
+    }
+}
