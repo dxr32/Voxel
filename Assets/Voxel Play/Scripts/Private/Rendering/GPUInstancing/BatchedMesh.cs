2e3b576d683dfa497a37355bd616308906c4f6ce
sedasandrit <55558448+sedasandrit@users.noreply.github.com>
2020-11-03 18:59:11 +0200

    VOXEL CHANGE
    CHANGE OTHER BRANCHES TO THIS

---
+++ Assets/Voxel Play/Scripts/Private/Rendering/GPUInstancing/BatchedMesh.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+namespace VoxelPlay.GPURendering.Instancing
+{
+
+    class BatchedMesh {
+		public VoxelDefinition voxelDefinition;
+		public Material material;
+		public FastList<Batch> batches;
+
+		public BatchedMesh(VoxelDefinition voxelDefinition) {
+			this.voxelDefinition = voxelDefinition;
+			batches = new FastList<Batch> ();
+		}
+	}
+}
